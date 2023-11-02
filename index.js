704e92a3bab118382ba27ef66ee2c034d189e1a6
muniquefeitoz4@gmail.com <muniquefeitoz4@gmail.com>
2023-11-02 12:00:27 -0300

    mudando de JavaScript para React

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-function createCloseButton(li) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.style.marginLeft = "20px";
-  
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
 
-  span.onclick = () => span.parentElement.style.display = "none";
-}
-
-document.querySelectorAll('li').forEach(createCloseButton);
-
-document.querySelector('ul').addEventListener('click', (e) => {
-  if (e.target.tagName === 'LI')
-    e.target.classList.toggle('checked');
-});
-
-function add() {
-  let li = document.createElement('LI');
-  let input_value = document.form_main.task.value;
-  let input_text = document.createTextNode(input_value);
-
-  li.appendChild(input_text);
-  document.querySelector('ul').appendChild(li);
-  document.form_main.task.value = "";
-
-  createCloseButton(li);
-}
+ReactDOM.render(<TodoList />, document.getElementById('root'));
